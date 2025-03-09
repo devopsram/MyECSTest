67edc54d037668f3623b3c29721bad1b5644e091
ram <ramveer.vm@gmail.com>
2025-03-09 16:47:42 +0530

    correction

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-1" # Change as needed
-}
-
 # VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
@@ -51,7 +47,10 @@ resource "aws_internet_gateway" "gw" {
 # Route Table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-  route { cidr_block = "0.0.0.0/0" gateway_id = aws_internet_gateway.gw.id }
+  route { 
+          cidr_block = "0.0.0.0/0" 
+          gateway_id = aws_internet_gateway.gw.id 
+        }
 }
 
 # Associate Route Table with Public Subnets
@@ -68,8 +67,18 @@ resource "aws_route_table_association" "public_2" {
 # Security Group for ECS Instances
 resource "aws_security_group" "ecs_sg" {
   vpc_id = aws_vpc.main.id
-  ingress { from_port = 80 to_port = 80 protocol = "tcp" cidr_blocks = ["0.0.0.0/0"] }
-  egress { from_port = 0 to_port = 0 protocol = "-1" cidr_blocks = ["0.0.0.0/0"] }
+  ingress { 
+            from_port = 80 
+            to_port = 80 
+            protocol = "tcp" 
+            cidr_blocks = ["0.0.0.0/0"] 
+          }
+  egress { 
+            from_port = 0 
+            to_port = 0     
+            protocol = "-1" 
+            cidr_blocks = ["0.0.0.0/0"] 
+         }
 }
 
 # ECS Cluster
